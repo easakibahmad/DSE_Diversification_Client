e8d95471a09f2ed3c324a0d0b8913e0fb368d989
easakibahmad <sakib.sgc.du@gmail.com>
2023-12-29 22:55:00 +0600

    navbar modified

--- src/Pages/Shared/Navbar.jsx
+++ src/Pages/Shared/Navbar.jsx
@@ -24,21 +24,16 @@ const Navbar = () => {
             className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
           >
             <li>
-              <a>Item 1</a>
+              <a>Our Services</a>
             </li>
             <li>
-              <a>Parent</a>
-              <ul className="p-2">
-                <li>
-                  <a>Submenu 1</a>
-                </li>
-                <li>
-                  <a>Submenu 2</a>
-                </li>
-              </ul>
+              <a>Investing Basics</a>
             </li>
             <li>
-              <a>Item 3</a>
+              <a>DSE Stocks</a>
+            </li>
+            <li>
+              <a>About Us</a>
             </li>
           </ul>
         </div>
@@ -47,28 +42,21 @@ const Navbar = () => {
       <div className="navbar-center hidden lg:flex">
         <ul className="menu menu-horizontal px-1">
           <li>
-            <a>Item 1</a>
+            <a>Our Services</a>
           </li>
           <li>
-            <details>
-              <summary>Parent</summary>
-              <ul className="p-2">
-                <li>
-                  <a>Submenu 1</a>
-                </li>
-                <li>
-                  <a>Submenu 2</a>
-                </li>
-              </ul>
-            </details>
+            <a>Investing Basics</a>
           </li>
           <li>
-            <a>Item 3</a>
+            <a>DSE Stocks</a>
+          </li>
+          <li>
+            <a>About Us</a>
           </li>
         </ul>
       </div>
       <div className="navbar-end">
-        <a className="btn">Button</a>
+        <a className="btn">Signup Now</a>
       </div>
     </div>
   );
